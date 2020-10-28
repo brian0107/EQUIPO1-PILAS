9f31270ad45f19f6852da02b08a953f93fb44052
BrianBecerra <brianbecerra65@gmail.com>
2020-10-28 12:52:07 -0600

    Agremamos codigo ejemplo de pilas en java

--- Programa/src/com/company/Main.java
+++ Programa/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Main {
+    /* Metodos
+    *push
+    * pop
+    * peek
+    * empty
+     */
 
     public static void main(String[] args) {
-	System.out.println("Equipo #1 Pilas");
+        //FILO first-in, last-out
+
+
+        Stack pila = new Stack();
+        pila.push(50);   //indice de 0
+        pila.push("String");// indice 1
+        pila.push(17);
+        pila.push("Palabra");
+        //Solo se puede obtener el ultimo valor
+        //peek para ver, y el metodo pop para obtener
+        System.out.println("El ultimo elemento en la pila es: "+pila.peek());
+
+        while(pila.empty()==false){
+            System.out.println(pila.pop());
+        }
+
     }
 }
