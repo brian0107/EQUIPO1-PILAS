277f2ad2b00e038ed55e9aaf87b7ec090381de08
BrianBecerra <brianbecerra65@gmail.com>
2020-10-28 19:29:23 -0600

    Actualizamos el programa

--- Programa/src/com/company/Main.java
+++ Programa/src/com/company/Main.java
@@ -3,29 +3,36 @@ package com.company;
 import java.util.Stack;
 
 public class Main {
-    /* Metodos
-    *push
-    * pop
-    * peek
-    * empty
+    /* 
+    Operaciones Básicas:
+    *push.- (Apilar) Agrega objetos a la pila.
+    * pop.- (Retirar) Retira el ultimo elemento de la pila.
+    Otras operaciones que podemos realizar:
+    * peek.- Obtener el ultimo elemento de la pila.
+    * empty.-True (si la pila no contiene elementos); False (si la pila contiene elementos).
+    * size.- Regresa el numero de elementos contenidos en la pila.
+    * search.- Realizamos la busqueda de un elemento.
      */
+
 
     public static void main(String[] args) {
         //FILO first-in, last-out
 
 
         Stack pila = new Stack();
-        pila.push(50);   //indice de 0
-        pila.push("String");// indice 1
+        pila.push(50);
+        pila.push("Juan");
         pila.push(17);
-        pila.push("Palabra");
-        //Solo se puede obtener el ultimo valor
-        //peek para ver, y el metodo pop para obtener
-        System.out.println("El ultimo elemento en la pila es: "+pila.peek());
+        pila.push("Pepe");
+        System.out.println("¿La pila esta vacia? "+pila.empty()+"\n");
+        System.out.println("El tamaño de la pila es : "+pila.size()+"\n");
+        System.out.println("El ultimo elemento en la pila es: "+pila.peek()+"\n");
+        System.out.println("Busqueda realizada: "+pila.search("Pepe")+"\n");
 
         while(pila.empty()==false){
-            System.out.println(pila.pop());
+            System.out.println("Eliminamos: "+pila.pop()+"\n");
         }
+        System.out.println("Imprimimos la pila: "+pila);
 
     }
 }
